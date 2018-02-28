f0e38acd842ba3716d0f9612a61a5e0436fdaae3
Electric Paper Evaluationssysteme GmbH <tal@evasys.de>
2018-02-28 17:56:45 +0100

    New files for Moodle Plug-in for EvaSys 2.0

---
+++ onlinesurvey/amd/src/modal-zoom.js
@@ -0,0 +1,149 @@
+define(['core/templates', 'core/str'], function(templates, str) {
+
+	var modalTitle = '';
+	var modalZoomSelector = '#surveys_content';
+	
+	var popupinfotitle = '';
+	var popupinfocontent = '';
+	var userlogintime = 0;
+	
+	var doRefresh = function() {
+		var myElement = document.getElementById("contentframe");
+		if(myElement){
+			var oldsrc = myElement.src;
+			myElement.src = '';
+			myElement.src = oldsrc;
+		}
+	}
+	
+	var handleClickSmallModal = function(e) {
+		e.preventDefault();
+
+		var modalZoomElem = e.target;
+
+		var originalIframe = modalZoomElem.querySelector('iframe');
+
+		var templatePromise = templates.render('block_onlinesurvey/modal-iframe', {
+			src: originalIframe.src,
+			title: modalTitle
+		});
+
+		templatePromise.done(function(source, javascript) {
+
+			var div = document.createElement('div');
+			div.innerHTML = source;
+
+			var modalContainer = div.firstChild;
+
+			document.body.insertBefore(modalContainer, document.body.firstChild);
+			document.body.className += ' custom-modal-open';
+
+			var closeCallback = function(e) {
+
+				var target = e.target
+
+				document.body.className = document.body.className.replace(' custom-modal-open', '');
+
+				doRefresh();
+
+				modalContainer.className += ' fading';
+
+				setTimeout(function() {
+					if(modalContainer.parentNode !== null) {
+						modalContainer.parentNode.removeChild(modalContainer);
+					}
+				}, 250);
+			}
+
+			modalContainer.querySelector('.custom-modal_close-button')
+				.addEventListener('click', function(e) {
+					e.preventDefault();
+					return closeCallback(e);
+				});
+
+			modalContainer.addEventListener('click', function(e) {
+				e.preventDefault();
+
+				return e.target !== modalContainer ?
+					false : closeCallback(e);
+			});
+		});
+	}
+	
+	return {
+		init: function(popuptitle, popupcontent, currentlogin) {
+
+			popupinfotitle = popuptitle;
+			popupinfocontent = popupcontent;
+			userlogintime = currentlogin;
+			
+			var zoomContainer = document.querySelectorAll(modalZoomSelector);
+
+			for	(var i=0; i<zoomContainer.length; i++) {
+				zoomContainer[i].addEventListener('click', handleClickSmallModal);
+			}
+			
+			// namespace in window for EVASYS-functions etc
+            window.EVASYS = {
+                // define "global" functions in namespace -> later "external" access from iframe possible
+                generate_popupinfo: this.generate_popupinfo
+            };
+            window.evasys_generate_popupinfo = this.generate_popupinfo;
+		},
+		generate_popupinfo: function() {
+			
+			// Get saved data from sessionStorage
+			var popupinfo = sessionStorage.getItem('onlinesurvey_popupinfo');
+			
+			if(popupinfo == false || popupinfo == null || popupinfo != userlogintime){
+				
+				// Save data to sessionStorage
+				sessionStorage.setItem('onlinesurvey_popupinfo', userlogintime);
+				
+				var templatePromise = templates.render('block_onlinesurvey/popupinfo', {
+					title: popupinfotitle,
+					content: popupinfocontent
+				});
+				
+				templatePromise.done(function(source, javascript) {
+					
+					var div = document.createElement('div');
+					div.innerHTML = source;
+					
+					var modalContainer = div.firstChild;
+					
+					document.body.insertBefore(modalContainer, document.body.firstChild);
+					document.body.className += ' custom-modal-open popupinfo';
+					
+					var closeCallback = function(e) {
+						
+						var target = e.target
+						
+						document.body.className = document.body.className.replace(' custom-modal-open', '');
+						
+						modalContainer.className += ' fading';
+						
+						setTimeout(function() {
+							if(modalContainer.parentNode !== null) {
+								modalContainer.parentNode.removeChild(modalContainer);
+							}
+						}, 250);
+					}
+					
+					modalContainer.querySelector('.custom-modal_close-button')
+					.addEventListener('click', function(e) {
+						e.preventDefault();
+						return closeCallback(e);
+					});
+					
+					modalContainer.addEventListener('click', function(e) {
+						e.preventDefault();
+						
+						return e.target !== modalContainer ?
+								false : closeCallback(e);
+					});
+				});
+			}
+		}
+	}
+});
