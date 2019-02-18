5b3eff093f59aa3694d20b3e2e3015ae068ea411
Alexander Bias <alexander.bias@uni-ulm.de>
2019-02-18 20:31:47 +0100

    Convert tabs to spaces

--- amd/src/modal-zoom.js
+++ amd/src/modal-zoom.js
@@ -1,37 +1,37 @@
 define(['core/templates', 'core/str'], function(templates, str) {
 
-	var modalTitle = '';
-	var modalZoomSelector = '#block_onlinesurvey_surveys_content';
-	
-	var popupinfotitle = '';
-	var popupinfocontent = '';
-	var userlogintime = 0;
-	
-	var doRefresh = function() {
-		var myElement = document.getElementById("block_onlinesurvey_contentframe");
-		if(myElement){
-			var oldsrc = myElement.src;
-			myElement.src = '';
-			myElement.src = oldsrc;
-		}
-	}
-	
-	var handleClickSmallModal = function(e) {
-		e.preventDefault();
+    var modalTitle = '';
+    var modalZoomSelector = '#block_onlinesurvey_surveys_content';
 
-		var modalZoomElem = e.target;
+    var popupinfotitle = '';
+    var popupinfocontent = '';
+    var userlogintime = 0;
 
-		var originalIframe = modalZoomElem.querySelector('iframe');
+    var doRefresh = function() {
+        var myElement = document.getElementById("block_onlinesurvey_contentframe");
+        if(myElement){
+            var oldsrc = myElement.src;
+            myElement.src = '';
+            myElement.src = oldsrc;
+        }
+    }
 
-		// #8984
-		var templatePromise = null;
+    var handleClickSmallModal = function(e) {
+        e.preventDefault();
+
+        var modalZoomElem = e.target;
+
+        var originalIframe = modalZoomElem.querySelector('iframe');
+
+        // #8984
+        var templatePromise = null;
 
 
         if (originalIframe !== null) {
-        	// open from Moodle page, i.e., onlinesurvey iframe exists
+            // open from Moodle page, i.e., onlinesurvey iframe exists
             templatePromise = templates.render('block_onlinesurvey/modal-iframe', {
                 // copy iframe target URL from block, but inform that now in modalZoom window
-            	src: originalIframe.src + "&modalZoom=1",
+                src: originalIframe.src + "&modalZoom=1",
                 title: modalTitle
             });
         } else {
@@ -39,128 +39,128 @@ define(['core/templates', 'core/str'], function(templates, str) {
             originalIframe = parent.document.querySelector('iframe');
             templatePromise = templates.render('block_onlinesurvey/modal-iframe', {
                 // copy iframe target URL from block, but inform that now in modalZoom window
-            	src: originalIframe.src + "&modalZoom=1",
+                src: originalIframe.src + "&modalZoom=1",
                 title: modalTitle
             });
         }
         // END #8984
 
-		templatePromise.done(function(source, javascript) {
+        templatePromise.done(function(source, javascript) {
 
-			var div = document.createElement('div');
-			div.innerHTML = source;
+            var div = document.createElement('div');
+            div.innerHTML = source;
 
-			var modalContainer = div.firstChild;
+            var modalContainer = div.firstChild;
 
-			document.body.insertBefore(modalContainer, document.body.firstChild);
-			document.body.className += ' block_onlinesurvey_custom-modal-open';
+            document.body.insertBefore(modalContainer, document.body.firstChild);
+            document.body.className += ' block_onlinesurvey_custom-modal-open';
 
-			var closeCallback = function(e) {
+            var closeCallback = function(e) {
 
-				var target = e.target
+                var target = e.target
 
-				document.body.className = document.body.className.replace(' block_onlinesurvey_custom-modal-open', '');
+                document.body.className = document.body.className.replace(' block_onlinesurvey_custom-modal-open', '');
 
-				doRefresh();
+                doRefresh();
 
-				modalContainer.className += ' fading';
+                modalContainer.className += ' fading';
 
-				setTimeout(function() {
-					if(modalContainer.parentNode !== null) {
-						modalContainer.parentNode.removeChild(modalContainer);
-					}
-				}, 250);
-			}
+                setTimeout(function() {
+                    if(modalContainer.parentNode !== null) {
+                        modalContainer.parentNode.removeChild(modalContainer);
+                    }
+                }, 250);
+            }
 
-			modalContainer.querySelector('.block_onlinesurvey_custom-modal_close-button')
-				.addEventListener('click', function(e) {
-					e.preventDefault();
-					return closeCallback(e);
-				});
+            modalContainer.querySelector('.block_onlinesurvey_custom-modal_close-button')
+                .addEventListener('click', function(e) {
+                    e.preventDefault();
+                    return closeCallback(e);
+                });
 
-			modalContainer.addEventListener('click', function(e) {
-				e.preventDefault();
+            modalContainer.addEventListener('click', function(e) {
+                e.preventDefault();
 
-				return e.target !== modalContainer ?
-					false : closeCallback(e);
-			});
-		});
-	}
-	
-	return {
-		init: function(popuptitle, popupcontent, currentlogin) {
+                return e.target !== modalContainer ?
+                    false : closeCallback(e);
+            });
+        });
+    }
 
-			popupinfotitle = popuptitle;
-			popupinfocontent = popupcontent;
-			userlogintime = currentlogin;
-			
-			var zoomContainer = document.querySelectorAll(modalZoomSelector);
+    return {
+        init: function(popuptitle, popupcontent, currentlogin) {
 
-			for	(var i=0; i<zoomContainer.length; i++) {
-				zoomContainer[i].addEventListener('click', handleClickSmallModal);
-			}
-			
-			// namespace in window for EVASYS-functions etc
+            popupinfotitle = popuptitle;
+            popupinfocontent = popupcontent;
+            userlogintime = currentlogin;
+
+            var zoomContainer = document.querySelectorAll(modalZoomSelector);
+
+            for (var i=0; i<zoomContainer.length; i++) {
+                zoomContainer[i].addEventListener('click', handleClickSmallModal);
+            }
+
+            // namespace in window for EVASYS-functions etc
             window.EVASYS = {
                 // define "global" functions in namespace -> later "external" access from iframe possible
-            		generate_popupinfo: this.generate_popupinfo
+                    generate_popupinfo: this.generate_popupinfo
             };
             window.evasysGeneratePopupinfo = this.generate_popupinfo;
-		},
-		generate_popupinfo: function() {
-			
-			// Get saved data from sessionStorage
-			var popupinfo = sessionStorage.getItem('onlinesurvey_popupinfo');
-			
-			if(popupinfo == false || popupinfo == null || popupinfo != userlogintime){
-				
-				// Save data to sessionStorage
-				sessionStorage.setItem('onlinesurvey_popupinfo', userlogintime);
-				
-				var templatePromise = templates.render('block_onlinesurvey/popupinfo', {
-					title: popupinfotitle,
-					content: popupinfocontent
-				});
-				
-				templatePromise.done(function(source, javascript) {
-					
-					var div = document.createElement('div');
-					div.innerHTML = source;
-					
-					var modalContainer = div.firstChild;
-					
-					document.body.insertBefore(modalContainer, document.body.firstChild);
-					document.body.className += ' block_onlinesurvey_custom-modal-open popupinfo';
-					
-					var closeCallback = function(e) {
-						
-						var target = e.target
-						
-						document.body.className = document.body.className.replace(' block_onlinesurvey_custom-modal-open', '');
-						
-						modalContainer.className += ' fading';
-						
-						setTimeout(function() {
-							if(modalContainer.parentNode !== null) {
-								modalContainer.parentNode.removeChild(modalContainer);
-							}
-						}, 250);
-					}
-					
-					modalContainer.querySelector('.block_onlinesurvey_custom-modal_close-button')
-					.addEventListener('click', function(e) {
-						e.preventDefault();
-						return closeCallback(e);
-					});
-					
-					modalContainer.addEventListener('click', function(e) {
-						e.preventDefault();
-						
-						return e.target !== modalContainer ?
-								false : closeCallback(e);
-					});
-				});
-			}
-		}
-	}
+        },
+        generate_popupinfo: function() {
+
+            // Get saved data from sessionStorage
+            var popupinfo = sessionStorage.getItem('onlinesurvey_popupinfo');
+
+            if(popupinfo == false || popupinfo == null || popupinfo != userlogintime){
+
+                // Save data to sessionStorage
+                sessionStorage.setItem('onlinesurvey_popupinfo', userlogintime);
+
+                var templatePromise = templates.render('block_onlinesurvey/popupinfo', {
+                    title: popupinfotitle,
+                    content: popupinfocontent
+                });
+
+                templatePromise.done(function(source, javascript) {
+
+                    var div = document.createElement('div');
+                    div.innerHTML = source;
+
+                    var modalContainer = div.firstChild;
+
+                    document.body.insertBefore(modalContainer, document.body.firstChild);
+                    document.body.className += ' block_onlinesurvey_custom-modal-open popupinfo';
+
+                    var closeCallback = function(e) {
+
+                        var target = e.target
+
+                        document.body.className = document.body.className.replace(' block_onlinesurvey_custom-modal-open', '');
+
+                        modalContainer.className += ' fading';
+
+                        setTimeout(function() {
+                            if(modalContainer.parentNode !== null) {
+                                modalContainer.parentNode.removeChild(modalContainer);
+                            }
+                        }, 250);
+                    }
+
+                    modalContainer.querySelector('.block_onlinesurvey_custom-modal_close-button')
+                    .addEventListener('click', function(e) {
+                        e.preventDefault();
+                        return closeCallback(e);
+                    });
+
+                    modalContainer.addEventListener('click', function(e) {
+                        e.preventDefault();
+
+                        return e.target !== modalContainer ?
+                                false : closeCallback(e);
+                    });
+                });
+            }
+        }
+    }
 });
