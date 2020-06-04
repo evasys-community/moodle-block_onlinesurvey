785f827666c9bc9ea7e5c80736ad88c707ba355f
Alexander Bias <alexander.bias@uni-ulm.de>
2020-06-04 20:44:04 +0200

    Release: Prepare develop branch for branching MOODLE_35_STABLE

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2020052200;
 $plugin->component = 'block_onlinesurvey';
-$plugin->release = 'v3.1-r1';
+$plugin->release = 'v3.5-r1';
 $plugin->requires = '2018051700';
 $plugin->maturity = MATURITY_STABLE;
