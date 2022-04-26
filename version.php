390e80cb63f1d3b3d9cfae0b293f3c4955c25c9d
Alexander Bias <alexander.bias@lernlink.de>
2022-04-26 11:37:52 +0200

    Release v3.11-r2

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2021112700;
+$plugin->version = 2021112701;
 $plugin->component = 'block_onlinesurvey';
-$plugin->release = 'v3.11-r1';
+$plugin->release = 'v3.11-r2';
 $plugin->requires = 2021051700;
 $plugin->supported = [311, 311];
 $plugin->maturity = MATURITY_STABLE;
