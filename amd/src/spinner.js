a9bf558eab67b8f97e914fce00f13cc3435c1a78
Josha Bartsch <>
2022-07-14 16:15:04 +0200

    loading icon is active, spawn ifram with src about:blank, set source by JS afterwards. This should fix an issue where sometimes the iframe was loaded before the 'load' eventlistener was set - resulting in the loading icon blocking the view.

--- amd/src/spinner.js
+++ amd/src/spinner.js
@@ -1,9 +1,10 @@
 define([], function() {
     return {
-        init: function() {
+        init: function(src) {
             var iframeElem = document.getElementById('block_onlinesurvey_contentframe');
             var contentElem = document.getElementById('block_onlinesurvey_surveys_content');
 
+            iframeElem.src = src;
             iframeElem.addEventListener('load', function() {
                 contentElem.className = contentElem.className.replace(/block_onlinesurvey_is-loading/, '');
             }, true);
