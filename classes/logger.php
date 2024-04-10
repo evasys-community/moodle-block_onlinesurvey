368a15fda63da3e8c68adc7710fa3b62f00a85f5
Pascal Collins <pascal.collins@iconnewmedia.de>
2024-04-10 09:59:05 +0200

    adds the possibility to specify the filename for the logger

--- classes/logger.php
+++ classes/logger.php
@@ -1,6 +1,14 @@
 <?php
 namespace block_onlinesurvey;
 class Logger {
+    protected $filename;
+    function __construct($filename = '') {
+        if (!empty($filename)) {
+            $this->filename = $filename;
+        } else {
+            $this->filename = 'log_lti.txt';
+        }
+    }
     function log($message, $obj = null) {
         global $CFG;
         if (!is_null($obj)) {
@@ -9,7 +17,7 @@ class Logger {
             $message .= "\r\n" . ob_get_clean();
         }
         file_put_contents(
-            $CFG->dataroot . '/log_lti.txt',
+            $CFG->dataroot . '/' . $this->filename,
             "\r\n### " . date('d.m.Y H:i:s') . "\r\n" . $message,
         FILE_APPEND
         );
