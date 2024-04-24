8d3abbd94e1b0e6d0938e331500a6b0881234ee2
Pascal Collins <pascal.collins@iconnewmedia.de>
2024-04-24 10:49:45 +0200

    comments the lines out again

--- show_surveys.php
+++ show_surveys.php
@@ -128,11 +128,11 @@ try {
                     if ($action === 'gradeReport') {
                         $msgtype = 'LtiSubmissionReviewRequest';
                     }
-                    if ($config->presentation == BLOCK_ONLINESURVEY_PRESENTATION_BRIEF) {
-                        echo block_onlinesurvey_lti_initiate_login_via_curl($config, $msgtype, '', '', $foruserid);
-                    } else {
+//                    if ($config->presentation == BLOCK_ONLINESURVEY_PRESENTATION_BRIEF) {
+//                        echo block_onlinesurvey_lti_initiate_login_via_curl($config, $msgtype, '', '', $foruserid);
+//                    } else {
                         echo block_onlinesurvey_lti_initiate_login($config, $msgtype, '', '', $foruserid);
-                    }
+//                    }
 
 //                    $SESSION->lti_initiatelogin_status = true;
                     exit;
