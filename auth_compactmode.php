2e33e969815003adaee28618f54524ef627273bf
PascalCollins <pascal.collins@iconnewmedia.de>
2025-06-27 18:15:30 +0200

    adds lots of logging for auth_compactmode.php

--- auth_compactmode.php
+++ auth_compactmode.php
@@ -22,12 +22,14 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 global $ok, $state, $DB, $CFG, $courseid, $id, $nonce, $messagetype, $foruserid, $typeid, $titleb64, $textb64, $error, $SESSION, $redirecturi;
+file_put_contents($CFG->dataroot . '/log_lti/auth_compactmode.txt', "\r\n" . __LINE__ . "\r\n", FILE_APPEND);
 if (isset($state)) {
     $SESSION->lti_state = $state;
 } else {
     $state = $SESSION->lti_state;
 }
 if ($ok) {
+    file_put_contents($CFG->dataroot . '/log_lti/auth_compactmode.txt', "\r\n" . __LINE__ . "\r\n", FILE_APPEND);
     $config = get_config('block_onlinesurvey');
     require_login();
     $context = context_system::instance();
@@ -36,37 +38,48 @@ if ($ok) {
     $params['state'] = $state;
     setcookie('state', $state, ['samesite' => 'None', 'path' => '/', 'expires' => time() + 30 * 24 * 3600]);
     setcookie('lti1p3_' . $state, $state, ['samesite' => 'None', 'path' => '/', 'expires' => time() + 30 * 24 * 3600]);
+    file_put_contents($CFG->dataroot . '/log_lti/auth_compactmode.txt', "\r\n" . __LINE__ . "\r\n", FILE_APPEND);
 } else {
     $params['error'] = $error;
+    file_put_contents($CFG->dataroot . '/log_lti/auth_compactmode.txt', "\r\n" . __LINE__ . "\r\nERROR: " . $error . "\r\n", FILE_APPEND);
     if (!empty($desc)) {
         $params['error_description'] = $desc;
     }
 }
-
+file_put_contents($CFG->dataroot . '/log_lti/auth_compactmode.txt', "\r\n" . __LINE__ . "\r\n", FILE_APPEND);
 $params['lti1p3_' . $SESSION->lti_state] = $SESSION->lti_state;
 if (isset($SESSION->lti_state)) {
+    file_put_contents($CFG->dataroot . '/log_lti/auth_compactmode.txt', "\r\n" . __LINE__ . "\r\n", FILE_APPEND);
     setcookie('lti1p3_' . $SESSION->lti_state, $SESSION->lti_state, time() + 30 * 24 * 3600, '/');
     block_onlinesurvey_remove_outdated_cookies($SESSION->lti_state);
+    file_put_contents($CFG->dataroot . '/log_lti/auth_compactmode.txt', "\r\n" . __LINE__ . "\r\n", FILE_APPEND);
 }
+file_put_contents($CFG->dataroot . '/log_lti/auth_compactmode.txt', "\r\n" . __LINE__ . "\r\n", FILE_APPEND);
 unset($SESSION->lti_message_hint);
 $config = block_onlinesurvey_get_launch_config();
-
+file_put_contents($CFG->dataroot . '/log_lti/auth_compactmode.txt', "\r\n" . __LINE__ . "\r\n", FILE_APPEND);
 $return = block_onlinesurvey_lti_post_launch_html_curl($params, $redirecturi, $config, $state);
 $modalzoom = 0;
+file_put_contents($CFG->dataroot . '/log_lti/auth_compactmode.txt', "\r\n" . __LINE__ . "\r\n", FILE_APPEND);
 if ($config->presentation == BLOCK_ONLINESURVEY_PRESENTATION_BRIEF) {
+    file_put_contents($CFG->dataroot . '/log_lti/auth_compactmode.txt', "\r\n" . __LINE__ . "\r\n", FILE_APPEND);
     if (isset($SESSION->modalzoom)) {
         $modalzoom = $SESSION->modalzoom;
         unset($SESSION->modalzoom);
     } else {
         $modalzoom = optional_param('modalZoom', 0, PARAM_INT);
     }
+    file_put_contents($CFG->dataroot . '/log_lti/auth_compactmode.txt', "\r\n" . __LINE__ . "\r\n", FILE_APPEND);
     if (!$modalzoom) {
+        file_put_contents($CFG->dataroot . '/log_lti/auth_compactmode.txt', "\r\n" . __LINE__ . "\r\n", FILE_APPEND);
         $return = block_onlinesurvey_get_summary($return, $config, $modalzoom, $foruserid);
         $return .= '<link rel="stylesheet" href="' .  $CFG->wwwroot . '/blocks/onlinesurvey/style/block_onlinesurvey_iframe_compact.css">';
         $return .= '<link rel="stylesheet" href="' . $CFG->wwwroot . '/blocks/onlinesurvey/lib/fonts/font-awesome-4.7.0/css/font-awesome.min.css">';
+        file_put_contents($CFG->dataroot . '/log_lti/auth_compactmode.txt', "\r\n" . __LINE__ . "\r\n", FILE_APPEND);
     }
 }
 if ($modalzoom || $config->presentation != BLOCK_ONLINESURVEY_PRESENTATION_BRIEF) {
+    file_put_contents($CFG->dataroot . '/log_lti/auth_compactmode.txt', "\r\n" . __LINE__ . "\r\n", FILE_APPEND);
     $pathinfo = pathinfo($config->lti_url);
     $base = $pathinfo['dirname'];
     if (strpos($return, '<head>') !== false) {
@@ -79,6 +92,7 @@ if ($modalzoom || $config->presentation != BLOCK_ONLINESURVEY_PRESENTATION_BRIEF
     } else {
         $cssselector = '#block_onlinesurvey_contentframe';
     }
+    file_put_contents($CFG->dataroot . '/log_lti/auth_compactmode.txt', "\r\n" . __LINE__ . "\r\n", FILE_APPEND);
     $return .= '<script>
 // make iframe height match its content
  var hassurveys = document.querySelectorAll(\'.cell.survey\').length > 0;
@@ -92,5 +106,5 @@ if (content_height > 0) {
 }
 </script>';
 }
-
+file_put_contents($CFG->dataroot . '/log_lti/auth_compactmode.txt', "\r\n" . __LINE__ . "\r\n", FILE_APPEND);
 echo $return;
